d4a62a2b1bb1627fe0ef084cdf8e1426ba5302da
Dennis Gillich <dennisgillich@live.de>
2022-03-04 04:09:34 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public byte Age { get; set; }
+
+    
+    public Person (string firstName, string lastName, byte age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+
+
+        static void printInformation(string firstName, string lastName, byte age)
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.WriteLine("Alter: " + age);
+        }
+        printInformation(firstName, lastName, age);
+    }
+
+
+
+}
+
---
+++ Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_10___Aufgabe_4
+{
+    class Lehrer : Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+    }
+
+}
