992c2e719a0d66ba5ace5a3efabf5437232ccffe
Dennis Gillich <dennisgillich@live.de>
2022-03-04 15:45:08 +0100

    Bessere Integration der Klassen, Aufgabe zu Ende geführt. Array integriert, foreach-Schleife und if-Abfrage integriert.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,80 @@
 ﻿
 
-class Person
+Person[] peopleInClassroom =
+{
+    new Lehrer("John", "Smith", 32),
+    new Student("Sabrina", "Müller", 18),
+    new Student("Anna", "Matt", 17),
+    new Student("Peter", "Fredl", 18),
+    new Student("Matthias", "Maier", 19)
+};
+foreach(Person p in peopleInClassroom)
+{
+    p.printInformation();
+
+    if(p is Lehrer)
+    {
+        (p as Lehrer).Teach();
+    }
+    else if ( p is Student)
+    {
+        (p as Student).ListenToTeacher();
+    }
+
+    Console.WriteLine();
+}
+
+Console.ReadKey();
+public class Person
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public byte Age { get; set; }
 
-    
-    public Person (string firstName, string lastName, byte age)
+
+    public Person(string firstName, string lastName, byte age)
     {
         FirstName = firstName;
         LastName = lastName;
         Age = age;
 
-
-        static void printInformation(string firstName, string lastName, byte age)
-        {
-            Console.WriteLine("Name: " + firstName + " " + lastName);
-            Console.WriteLine("Alter: " + age);
-        }
-        printInformation(firstName, lastName, age);
+    }
+    public void printInformation()
+    {
+        Console.WriteLine("Name: " + FirstName + " " + LastName);
+        Console.WriteLine("Alter: " + Age);
     }
 
 
 
+}
+class Lehrer : Person
+{
+    public Lehrer(string firstName, string lastName, byte age) : base(firstName, lastName, age)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        age = Age;
+    }
+
+    public void Teach()
+    {
+        Console.WriteLine("Herr/Frau {0} unterrichtet die Klasse!", LastName);
+    }
+
+}
+class Student : Person
+{
+    public Student(string firstName, string lastName, byte age) : base(firstName, lastName, age)
+    {
+        firstName = FirstName;
+        lastName = LastName;
+        age = Age;
+    }
+
+    public void ListenToTeacher()
+    {
+        Console.WriteLine("Der/Die Schüler/in {0} {1} hört dem Lehrer zu!", FirstName, LastName);
+    }
 }
 
